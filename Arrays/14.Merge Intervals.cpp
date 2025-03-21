8ef238beffd7d144983326bebf1e49853929a29a
Vivek <vivek261125@gmail..com>
2025-03-22 00:20:06 +0530

    new

--- Arrays/14.Merge Intervals.cpp
+++ Arrays/14.Merge Intervals.cpp
@@ -1,7 +1,7 @@
 class Solution {
     public:
         vector<vector<int>> merge(vector<vector<int>>& intervals) {
-           t
+           
             sort(intervals.begin(), intervals.end());
             
             vector<vector<int>> ans;
